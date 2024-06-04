652f12d02bd9fce888a8b7df1b2af6e7e0f3b8ee
mekontso <mekontsoanderson@gmail.com>
2024-06-05 00:56:38 +0100

    Initial commit

---
+++ src/test/java/com/mas/app/BusreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
